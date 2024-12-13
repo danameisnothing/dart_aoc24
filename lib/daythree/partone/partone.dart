35d8d9e0dd8f0ebc74bac749725645f2c985f978
danameisnothing <test720now@gmail.com>
2024-12-13 17:07:02 +0700

    added some functions starting from day 2 part 2, and day 3 part 1

---
+++ lib/daythree/partone/partone.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void processInput(String input) {
+
+}
+
+void exec() {
+  final input = File("lib/daythree/partone/input.txt").readAsStringSync();
+
+  final filter = RegExp(r"mul\(\d+,\d+\)");
+  final matches = filter.allMatches(input);
+
+  int total = 0;
+
+  for (RegExpMatch match in matches) {
+    final instruction = input.substring(match.start, match.end).trim();
+
+    final digits = RegExp(r"\d+").allMatches(instruction);
+
+    final first = int.parse(instruction.substring(digits.first.start, digits.first.end).trim());
+    final second = int.parse(instruction.substring(digits.last.start, digits.last.end).trim());
+
+    total += first * second;
+  }
+
+  print(total);
+}
--- lib/daytwo/parttwo/parttwo.dart
+++ lib/daytwo/parttwo/parttwo.dart
@@ -1,11 +1,10 @@
 import 'dart:io';
 
-void exec() {
-  final input = File("lib/daytwo/parttwo/input.txt").readAsLinesSync();
+const faultTolerance = 1;
 
+List<List<int>> processInput(List<String> lines) {
   final reports = <List<int>>[];
-
-  for (String str in input) {
+  for (String str in lines) {
     str = str.trim(); // yikes
     
     final matches = RegExp(r"\S+").allMatches(str);
@@ -17,57 +16,91 @@ void exec() {
     }
   }
 
+  return reports;
+}
+
+bool getLevelIncreasingTrend(List<int> report) {
+  final areReportsIncreasing = <bool>[];
+  for (int i = 1; i < report.length; i++) {
+    final elBefore = report.elementAt(i - 1);
+    final elNow = report.elementAt(i);
+
+    final delta = elNow - elBefore;
+
+    // Only do this at start of each report
+    areReportsIncreasing.add((!delta.isNegative) ? true : false);
+  }
+
+  // The first thing I've thought of that is easy-ish, to deal with the assumption that the start delta of each reports can have a wrong sign
+  final howManyTrue = areReportsIncreasing.where((x) => x == true).length;
+  final howManyFalse = areReportsIncreasing.where((x) => x == false).length;
+
+  return (!howManyTrue.compareTo(howManyFalse).isNegative) ? true : false;
+}
+
+void exec() {
+  final reports = processInput(File("lib/daytwo/parttwo/input.txt").readAsLinesSync());
+
   int total = 0;
 
   for (List<int> report in reports) {
-    final areReportsIncreasing = <bool>[];
-    int usedChance = 0;
-    for (int i = 1; i < report.length; i++) {
-      final elBefore = report.elementAt(i - 1);
-      final elNow = report.elementAt(i);
+    int totalViolations = 0;
+    bool isReportIncreasing = getLevelIncreasingTrend(report);
+    print(isReportIncreasing);
 
-      final delta = elNow - elBefore;
+    // Try elBefore deletion, then elNow deletion. If both have a fault value above 1, then ignore
+    // FIXME: CLEANUP
+    final backupReport = List.from(report); // So it copies
+    bool detectedFault = false;
+    print("Trying elNow deletion method");
+    while (true) {
+      detectedFault = false;
+      for (int i = 1; i < report.length; i++) {
+        final elBefore = report.elementAt(i - 1);
+        final elNow = report.elementAt(i);
 
-      // Only do this at start of each report
-      areReportsIncreasing.add((!delta.isNegative && delta.sign != 0) ? true : false);
-    }
+        final delta = elNow - elBefore;
 
-    // The first thing I've thought of that is easy-ish
-    // To deal with the wrong values at start
-    final howManyTrue = areReportsIncreasing.where((x) => x == true).length;
-    final howManyFalse = areReportsIncreasing.where((x) => x == false).length;
-
-    assert(howManyTrue.compareTo(howManyFalse) != 0, "Report $report has same amount of instances where value is increasing and decreasing.");
-
-    bool areMajorityReportsIncreasing = (!howManyTrue.compareTo(howManyFalse).isNegative) ? true : false;
-
-    print("$areReportsIncreasing for report $report with result is $areMajorityReportsIncreasing for areMajorityIncreasing");
-
-    continue;
-
-    /*for (int i = 1; i < report.length; i++) {
-      final elBefore = report.elementAt(i - 1);
-      final elNow = report.elementAt(i);
-
-      final delta = elNow - elBefore;
-
-      // Only do this at start of each report
-      if (i == 1) {
-        if (!delta.isNegative) {
-          isReportIncreasing = true;
+        // Check if sign is the same as initial pattern || Checks if adjacent levels differ within the threshold, the "is neither an increase or a decrease" case is already handled here
+        if ((!delta.isNegative != isReportIncreasing) || (delta.abs() < 1 || delta.abs() > 3)) {
+          print("Deleting $elNow at index $i");
+          report.removeAt(i); // elNow
+          totalViolations++;
+          detectedFault = true;
+          break;
         }
       }
+      if (!detectedFault) break;
+    }
 
-      // Check if sign is the same as initial pattern || Checks if adjacent levels differ within the threshold, the "is neither an increase or a decrease" case is already handled here
-      if ((!delta.isNegative != isReportIncreasing) || (delta.abs() < 1 || delta.abs() > 3)) {
-        report.removeAt(i);
-        i--;
-        usedChance++;
-        print("$usedChance");
+    if (totalViolations > faultTolerance) {
+      totalViolations = 0;
+      print("Trying elBefore deletion method");
+      report = List.from(backupReport);
+      while (true) {
+        detectedFault = false;
+        for (int i = 1; i < report.length; i++) {
+          final elBefore = report.elementAt(i - 1);
+          final elNow = report.elementAt(i);
+
+          final delta = elNow - elBefore;
+
+          // Check if sign is the same as initial pattern || Checks if adjacent levels differ within the threshold, the "is neither an increase or a decrease" case is already handled here
+          if ((!delta.isNegative != isReportIncreasing) || (delta.abs() < 1 || delta.abs() > 3)) {
+            print("Deleting $elBefore at index ${i - 1}");
+            report.removeAt(i - 1); // elBefore
+            totalViolations++;
+            detectedFault = true;
+            break;
+          }
+        }
+        if (!detectedFault) break;
       }
-    }*/
-    if (usedChance <= 1) total++;
+    }
+
+    print("$report : $totalViolations");
+    if (totalViolations <= faultTolerance) total++;
   }
 
-  //print(total);
+  print(total);
 }
